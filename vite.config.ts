36f804b32525d6f0e9cc3fa1297d15e3b1104619
Thilina Lakshan <thilinalakshanmail@gmail.com>
2024-12-26 04:26:59 +0530

    Add tailwindcss and shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { keycloakify } from "keycloakify/vite-plugin";
+import * as path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,11 +10,16 @@ export default defineConfig({
     keycloakify({
       accountThemeImplementation: "none",
       keycloakVersionTargets: {
-        "all-other-versions": "mint-theme.jar",
-        "22-to-25": false
+        "all-other-versions": "shadcn-theme.jar",
+        "22-to-25": false,
       },
-      themeName: "mint-theme",
+      themeName: "shadcn-theme",
       themeVersion: "1.0.0"
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 });
